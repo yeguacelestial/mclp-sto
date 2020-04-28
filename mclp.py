364d721350400574f369906dcd65ef1dc9546981
Yegua Celestial <yeguacelestial@gmail.com>
2020-04-28 13:40:46 -0500

    Created mclp and updated TODO

---
+++ mclp.py
@@ -0,0 +1,17 @@
+# INPUT:
+# * Number of sites to select >> K
+# * Service radius of each site >> radius
+# * Candidate site size (Desired population to cover) >> M
+#
+# OUTPUT:
+# * Objective function of Constructive Heuristic -> Total of the population covered
+# * Execution time of the Constructive Heuristic -> cpu_sec_ch
+# * Objective function of Local Search Heuristic -> Total of the population covered IMPROVED
+# * Execution time of the Local Search Heuristic -> cpu_sec_ls
+# TODO: Constructive Heuristic
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
