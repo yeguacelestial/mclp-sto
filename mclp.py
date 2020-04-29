21fb10f179a68d349b53024341421460694e02db
Yegua Celestial <yeguacelestial@gmail.com>
2020-04-29 15:58:27 -0500

    Created TypeError exception

--- mclp.py
+++ mclp.py
@@ -69,6 +69,9 @@ def main():
     except FileNotFoundError:
         print("[-] Error: File not found.")
     
+    except TypeError:
+        print("[-] Error: input couldn't be read.")
+
     except Exception as e:
         raise
         #print("[-] Error: something went wrong.")
